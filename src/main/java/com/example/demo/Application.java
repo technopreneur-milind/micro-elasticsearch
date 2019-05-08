5c8f8fbd0975c69c823d81f9ee9488a1fe4c4ef1
Milind Joshi <Milind Joshi>
2019-05-08 06:24:35 +0530

    add

---
+++ src/main/java/com/example/demo/Application.java
@@ -0,0 +1,67 @@
+package com.example.demo;
+
+import com.example.demo.book.model.Book;
+import com.example.demo.book.service.BookService;
+
+import org.elasticsearch.client.Client;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
+
+import java.util.List;
+import java.util.Map;
+
+@SpringBootApplication
+public class Application implements CommandLineRunner {
+
+    @Autowired
+    private ElasticsearchOperations es;
+
+    @Autowired
+    private BookService bookService;
+
+    public static void main(String args[]) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        printElasticSearchInfo();
+
+        bookService.save(new Book("1001", "Elasticsearch Basics", "Mr X", "23-FEB-2017"));
+        bookService.save(new Book("1002", "Apache Lucene Basics", "Mr X", "13-MAR-2017"));
+        bookService.save(new Book("1003", "Apache Solr Basics", "Mr X", "21-MAR-2017"));
+
+        //fuzzey search
+        Page<Book> books = bookService.findByAuthor("X", new PageRequest(0, 10));
+
+        List<Book> books2 = bookService.findByTitle("Apache");
+        
+        System.out.println("Printing Books for X Author");
+        books.forEach(x -> System.out.println(x));
+        
+        System.out.println("Printing Books with title Apache");
+        books2.forEach(x -> System.out.println(x));
+
+
+    }
+
+    //useful for debug
+    private void printElasticSearchInfo() {
+
+        System.out.println("--ElasticSearch-->");
+        Client client = es.getClient();
+        Map<String, String> asMap = client.settings().getAsMap();
+
+        asMap.forEach((k, v) -> {
+            System.out.println(k + " = " + v);
+        });
+        System.out.println("<--ElasticSearch--");
+    }
+
+}
